3d231c9ba15fece37bfff9abdc80c6db431dd690
aysha945 <76861040+aysha945@users.noreply.github.com>
2021-08-28 23:47:26 +0530

    day 3 assignments

---
+++ javascript assigment 3/jumble.js
@@ -0,0 +1,81 @@
+let words=
+[
+    {
+        jumbled:"gamien",
+        correct:"enigma"
+    },
+    {
+        jumbled:"vlretarel",
+        correct:"traveller"
+    },
+    {
+        jumbled:"bangle",
+        correct:"gnlbae"
+    },
+    {
+        jumbled:"srviu",
+        correct:"virus"
+    },
+    {
+        jumbled:"ponehrae",
+        correct:"earphone"
+    },
+
+]
+
+let currentWord=0;
+let score=0;
+
+function displayWord()
+{
+    document.querySelector("#j-word").innerText = 
+    words[currentWord].jumbled;
+
+    document.querySelector("#score").innerText=score;
+}
+
+
+displayWord();
+
+
+function checkAnswer()
+{
+    
+            let answer=document.querySelector("#inp").value;
+
+            if(answer.toLowerCase()===words[currentWord].correct.toLowerCase()){
+
+                score++;
+
+            }
+
+            if(currentWord==words.length-1){
+
+                let msg="";
+
+                if(score===5){
+                    msg="Amazing game "+score;
+                }
+                else if(score===4){
+                    msg="Good Game "+score
+                }
+                else if(score<4){
+                    msg="Good Try "+score
+                }
+
+                alert(msg);
+
+                location.reload();
+            }
+            else{
+                currentWord++;
+                displayWord();
+                document.querySelector("#inp").value="";
+            }
+
+            
+
+    
+}
+
+
